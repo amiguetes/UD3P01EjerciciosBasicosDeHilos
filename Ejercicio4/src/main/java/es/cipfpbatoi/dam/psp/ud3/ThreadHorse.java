745add4626cbf9807794c3d5c7caad699d4ad871
Arturo Candela <a.candelamolto@edu.gva.es>
2024-02-03 16:43:15 +0100

    Creado modulo Ejercicio4 refs #1

---
+++ Ejercicio4/src/main/java/es/cipfpbatoi/dam/psp/ud3/ThreadHorse.java
@@ -0,0 +1,32 @@
+package es.cipfpbatoi.dam.psp.ud3;
+
+public class ThreadHorse implements Runnable{
+
+    private final int DISTANCIA_TOTAL_METROS; // Distancia en metros
+    private final int PASO_METROS ; // Cada paso en metros
+    private final int DESCANSO_MS; // Descanso de entre pasos
+
+    public ThreadHorse(int paso, int descanso, int distanciaTotal){
+        this.PASO_METROS = paso;
+        this.DESCANSO_MS = descanso;
+        this.DISTANCIA_TOTAL_METROS = distanciaTotal;
+    }
+
+    public ThreadHorse(){
+        this(100,200,5000);
+    }
+
+    public void run() {
+        try {
+            int distanciaRestante = DISTANCIA_TOTAL_METROS;
+            while (distanciaRestante > 0) {
+                distanciaRestante -= PASO_METROS;
+                System.out.println(Thread.currentThread().getName() + ": " + distanciaRestante + "m para finalizar");
+                Thread.sleep(DESCANSO_MS);
+            }
+            System.out.println(Thread.currentThread().getName() + ": Ha Finalizado");
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + " fue interrumpido.");
+        }
+    }
+}
